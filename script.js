8f46cca979860a496f22b0d023a3bd686c595c6f
Gromit <eversoryan@gmail.com>
2022-05-09 23:32:30 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Query Selectors go here
+var mainHeader = document.querySelector(h1);
+
+// Regualr variables go here
+var selection
+
+//Init function that starts on page load
+function init() {
+    getScore();
+}
+// Function for the questions and answers
+function startQuiz() {
+
+}
+
+// Function for keeping score in local storage
+function getScore() {
+
+}
+
+// Function for final screen that displays score
+function highscore() {
+    
+}
+
+// Function that activates on button click
+// Will call the other functions in this one
+function gameStart() {
+
+}
