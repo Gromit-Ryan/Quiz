e4a6e19219a1c614b57bf70891865f417fb377d5
Gromit <eversoryan@gmail.com>
2022-05-10 16:33:37 -0600

    Added some query selectors

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Query Selectors go here
-var mainHeader = document.querySelector(h1);
+var answer1 = document.querySelector('#answer1');
+var answer2 = document.querySelector('#answer2');
+var answer3 = document.querySelector('#answer3');
+var answer4 = document.querySelector('#answer4');
+var viewHighscore = document.querySelector('#viewHighscore');
+var timeLeft = document.querySelector('#timeLeft');
+var questions = document.querySelector('#questions');
 
 // Regualr variables go here
 var selection
@@ -8,6 +14,12 @@ var selection
 function init() {
     getScore();
 }
+
+// Function for the time left
+function timeRemaining() {
+    
+}
+
 // Function for the questions and answers
 function startQuiz() {
 
